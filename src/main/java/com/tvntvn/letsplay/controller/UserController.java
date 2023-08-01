cbd11ee6fe7a2f9c4f44dbf949bc203374703b43
Taneli Mäkihannu <tvntvn@mba.home>
2023-08-01 16:09:31 +0300

    database connection works
    changed the endpoint url

--- src/main/java/com/tvntvn/letsplay/controller/UserController.java
+++ src/main/java/com/tvntvn/letsplay/controller/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("api/users")
 public class UserController {
   @Autowired private UserService service;
 
