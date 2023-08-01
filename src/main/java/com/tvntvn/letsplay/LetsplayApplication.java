309139a361ed3f2fef87bfc1ad75ced1b8513c38
Taneli Mäkihannu <tvntvn@mba.home>
2023-08-01 12:30:31 +0300

    init

---
+++ src/main/java/com/tvntvn/letsplay/LetsplayApplication.java
@@ -0,0 +1,13 @@
+package com.tvntvn.letsplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tvntvn/letsplay/LetsplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvntvn.letsplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
