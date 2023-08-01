acc6d3ac16ab237751c52b46240be7480790ebec
tvntvn13 <tnlmkhnn@me.com>
2023-08-01 18:37:56 +0300

    changed the endpoint url

--- src/main/java/com/tvntvn/letsplay/controller/ProductController.java
+++ src/main/java/com/tvntvn/letsplay/controller/ProductController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("api/products")
 public class ProductController {
   @Autowired private ProductService service;
 
--- src/main/java/com/tvntvn/letsplay/controller/UserController.java
+++ src/main/java/com/tvntvn/letsplay/controller/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("api/users")
 public class UserController {
   @Autowired private UserService service;
 
