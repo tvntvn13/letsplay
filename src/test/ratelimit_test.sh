bd0ad6c03cbf933c88471fa04240bee021d30895
tvntvn13 <tnlmkhnn@me.com>
2023-08-29 15:58:00 +0300

    change ratelimit test to https
    deleted the pass test bc they are now combined in one

--- src/test/ratelimit_test.sh
+++ src/test/ratelimit_test.sh
@@ -3,7 +3,7 @@
 echo "sending 15 consecutive requests - some should fail"
 sleep 1
 for i in {1..15}; do
-	curl --head --silent -X GET http://localhost:8080/api/products | awk '/HTTP/{print $2}' &
+	curl --head --silent -k -X GET https://localhost:443/api/products | awk '/HTTP/{print $2}' &
 done
 
 wait
@@ -12,15 +12,15 @@ echo ""
 echo "sending 15 request with 1 second wait between - should not fail"
 sleep 1
 for i in {1..5}; do
-	curl --head --silent -X GET http://localhost:8080/api/products | awk '/HTTP/{print $2}' &
+	curl --head --silent -k -X GET https://localhost:443/api/products | awk '/HTTP/{print $2}' &
 done
 sleep 1
 for i in {1..5}; do
-	curl --head --silent -X GET http://localhost:8080/api/products | awk '/HTTP/{print $2}' &
+	curl --head --silent -k -X GET https://localhost:443/api/products | awk '/HTTP/{print $2}' &
 done
 sleep 1
 for i in {1..5}; do
-	curl --head --silent -X GET http://localhost:8080/api/products | awk '/HTTP/{print $2}' &
+	curl --head --silent -k -X GET https://localhost:443/api/products | awk '/HTTP/{print $2}' &
 done
 
 wait
