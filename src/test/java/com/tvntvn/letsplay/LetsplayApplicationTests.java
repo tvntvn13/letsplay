392a0c1211628cec96e46b08ee6545fc9ad7f2ff
tvntvn13 <tnlmkhnn@me.com>
2025-01-23 22:58:51 +0200

    refactor: make updateuser more readable

--- src/test/java/com/tvntvn/letsplay/LetsplayApplicationTests.java
+++ src/test/java/com/tvntvn/letsplay/LetsplayApplicationTests.java
@@ -1,13 +1,15 @@
 package com.tvntvn.letsplay;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LetsplayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {
+    assertEquals(3, 1 + 2);
+  }
 }
